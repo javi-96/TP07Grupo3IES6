293737906a6ff76ecc74637dcab20c251104dc34
Javier Choque <alejandroj889@gmail.com>
2024-11-05 10:57:11 -0300

    Inicializando tp07 - Javier Choque

---
+++ src/main/java/ar/edu/ies6/tp07/Tp07Ies6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tp07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp07Ies6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp07Ies6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/ies6/tp07/Tp07Ies6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tp07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp07Ies6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
